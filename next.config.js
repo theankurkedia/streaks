0d9eeb78444397cd4a4e7d825e03fe73d9bbe326
Ankur Kedia <theankurkedia@gmail.com>
2025-01-09 21:49:08 +0100

    chore: config changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const { withExpo } = require('@expo/next-adapter');
 
 module.exports = withExpo({
-  // Your custom Next.js config options here
-  transpilePackages: [
-    'react-native',
-    'expo',
-    // Add any other packages that need transpiling
-  ],
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ['react-native', 'react-native-web', 'expo'],
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  distDir: '.next',
 });
